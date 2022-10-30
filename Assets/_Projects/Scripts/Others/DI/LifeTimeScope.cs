2a3d031fda74c7966f29842ac8cc7a1e68df55d3
IShix <49128115+IShix-g@users.noreply.github.com>
2022-10-30 22:04:00 +0900

    ライセンスの更新

--- Assets/_Projects/Scripts/Others/DI/LifeTimeScope.cs
+++ Assets/_Projects/Scripts/Others/DI/LifeTimeScope.cs
@@ -1,5 +1,9 @@
+// SMCPxPuzzleGame:
 // Copyright (c) 2022 IShix All rights reserved.
 // Licensed under the GNU Affero General Public License v3.0. See LICENSE file in the project root for full license information.
+// VContainer:
+// Copyright (c) 2020 hadashiA
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 using LogicAndModel;
 using VContainer;
